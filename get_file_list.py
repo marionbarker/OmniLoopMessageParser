58e0722961bd1dd8b7b5e82c9c5f8ffb0cab862c
Marion <marion@pauls-MBP.attlocal.net>
2019-02-22 07:59:03 -0800

    Modify code to return sorted list of relative filename, creation date

--- get_file_list.py
+++ get_file_list.py
@@ -3,13 +3,14 @@ from os import listdir
 
 def get_file_list(thisPath):
     """
-    Returns a list of folders and their associated *.md files in a list
+    Returns a filenames with associated creation times in a list
 
     PARAMS:
-        path folder where the MessageLog files by person are stored
+        thisPath: path folder where the MessageLog files by person are stored
 
     RETURNS:
-        list_of_files including subfolders
+        returnList list of filename(relative to thisPath), creation time for file
+                where returnList is sorted by creation time
     """
     list_of_reports = []
     list_of_dates = []
@@ -21,4 +22,9 @@ def get_file_list(thisPath):
             list_of_reports.append(thisFile)
             list_of_dates.append(os.path.getmtime(thisPath + '/' + thisFile))
 
-    return (list_of_reports, list_of_dates)
+    zippedTuple = zip(list_of_reports, list_of_dates)
+    zippedList = list(zippedTuple)
+    returnList = sorted(zippedList, key = lambda x: x[1])
+
+    # return the list of files and dates sorted by date
+    return returnList
