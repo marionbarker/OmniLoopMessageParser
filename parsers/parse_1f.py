ffd51db675936403b30ac8f629e0ba6843aec87c
marionbarker <marionbarker@earthlink.net>
2020-07-04 19:21:29 -0700

    move test input files and example output to folder named test

--- parsers/parse_1f.py
+++ parsers/parse_1f.py
@@ -1,6 +1,6 @@
 # file: parse_1f - does the parsing for cancel commond
 from util.misc import combineByte
-from decimal import Decimal
+
 
 def parse_1f(byteList, msgDict):
     # extract information from the 1f cancel command
@@ -15,23 +15,31 @@ def parse_1f(byteList, msgDict):
     05 (1 byte): The cancel command always has a fixed length of 05
     NNNNNNNN (4 bytes): Nonce, the 32-bit validator (random looking numbers)
     AX (1 byte): Command byte of the form aaaa0bcd:
-        The aaaa (A) nibble in the range of (0..8) and is the type of the Pod alarm/beep type to sound.
+        The aaaa (A) nibble in the range of (0..8) and is the type of
+                      the Pod alarm/beep type to sound.
             An aaaa value = 0 is no alarm
                           = 2 two quick beeps
                           = 6 one longer beep
-            Values of A larger than 8 are out of range and can lock up a Pod with a constant beep.
-        The b ($04) bit being set will cancel any ongoing bolus and will turn off the RR reminder variables set from the $17 Bolus Extra command.
-        The c ($02) bit being set will cancel any ongoing temporary basal and will turn off the RR reminder variables set from the $16 Temp Basal command.
-        The d ($01) bit being set will cancel the basal program and will turn off the RR reminder variables set from the $13 Basal Schedule command.
+            Values of A larger than 8 are out of range and can lock up
+                a Pod with a constant beep.
+        The b ($04) bit being set will cancel any ongoing bolus and
+            will turn off the RR reminder variables set from the
+            $17 Bolus Extra command.
+        The c ($02) bit being set will cancel any ongoing temporary basal
+            and will turn off the RR reminder variables set from the
+            $16 Temp Basal command.
+        The d ($01) bit being set will cancel the basal program and will
+            turn off the RR reminder variables set from the
+            $13 Basal Schedule command.
     The Pod responds to the $1F command with a $1D status message.
     """
 
     cancelByte = byteList[6]
     alertValue = (cancelByte >> 4) & 0xF
     cancelBolus = (cancelByte & 0x04) != 0
-    cancelTB    = (cancelByte & 0x02) != 0
-    suspend     = (cancelByte & 0x01) != 0
-    cnxByteStr = '{0:x}'.format(cancelByte,2)
+    cancelTB = (cancelByte & 0x02) != 0
+    suspend = (cancelByte & 0x01) != 0
+    cnxByteStr = '{0:x}'.format(cancelByte, 2)
     msgDict['msgType'] = msgDict['msgType']+cnxByteStr
 
     if cancelByte == 7:
--- test.py
+++ test.py
@@ -1,16 +1,16 @@
-# run the example files - output to screen should match corresponding example output files
-# usage:  python test.py > exampleFiles/exampleOutput_test.txt
-from main import *
-import pandas as pd
+# run the example files - output to screen should text in
+#  test/exampleOutput.txt file
+# usage:  python test.py
+from main import main
 
 outFile = 0
 vFlag = 0
 filePath = '.'
 
-## Persistent log version, multiple pod session:
-thisFile = 'exampleFiles/DeviceCommunicationsLogExample_multiplePods.md'
+# Persistent log version, multiple pod session:
+thisFile = 'test/DeviceCommunicationsLogExample_multiplePods.md'
 main(filePath, thisFile, outFile, vFlag)
 
-## old-style log version, single pod session:
-thisFile = 'exampleFiles/MessageLogExample_singlePod.md'
+# old-style log version, single pod session:
+thisFile = 'test/MessageLogExample_singlePod.md'
 main(filePath, thisFile, outFile, vFlag)
