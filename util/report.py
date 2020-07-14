a7751be5a7a4624faf97904f55730c450537021f
marionbarker <marionbarker@earthlink.net>
2020-07-13 21:22:14 -0700

    clean up format, add uncategorized messages report

--- util/report.py
+++ util/report.py
@@ -1,30 +1,35 @@
 import pandas as pd
 import numpy as np
 import os
-from util.pod import *
+from util.pod import getDescriptiveStringFromPodStateRow, getNameFromMsgType
+from util.misc import printDict
 
 """
     This code has functions to handle reporting to stdout or files
 """
 
+
 def printActionSummary(actionSummary, vFlag):
     # initialize values just in case
-    numShortTB = np.nan
-    numSchBasalbeforeTB = np.nan
-    numRepeatedTB = np.nan
-    print('\n  Action Summary with sequential 4 or 2 message sequences with action response times in sec')
-    print('      Action        : #Success,  mean, [  min,  max  ] : #Incomplete')
-    #actionSummary
-    #printDict(actionSummary)
+    # numShortTB = np.nan
+    # numSchBasalbeforeTB = np.nan
+    # numRepeatedTB = np.nan
+    print('\n  Action Summary with sequential 4 or 2 message sequences with'
+          ' action response times in sec')
+    print('      Action        : #Success,  mean, [  min,  max  ] : '
+          '#Incomplete')
+    # actionSummary
+    # printDict(actionSummary)
 
     for keys, values in actionSummary.items():
         subDict = values
-        print('    {:14s}  :  {:5.0f},  {:5.0f},  [{:5.0f}, {:5.0f} ] : {:5d}'.format( \
-          keys, subDict['countCompleted'], subDict['meanResponseTime'], \
-          subDict['minResponseTime'], subDict['maxResponseTime'], \
-          subDict['countIncomplete']))
+        print('    {:14s}  :  {:5.0f},  {:5.0f},  [{:5.0f}, {:5.0f} ] '
+              ': {:5d}'.format(
+               keys, subDict['countCompleted'], subDict['meanResponseTime'],
+               subDict['minResponseTime'], subDict['maxResponseTime'],
+               subDict['countIncomplete']))
         # deprecated (Pete fixed code)
-        #if keys=='CnxSetTmpBasal':
+        # if keys=='CnxSetTmpBasal':
         #    numShortTB          = subDict['numShortTB']
         #    numSchBasalbeforeTB = subDict['numSchBasalbeforeTB']
         #    numRepeatedTB       = subDict['numRepeatedTB']
@@ -32,36 +37,47 @@ def printActionSummary(actionSummary, vFlag):
         #    numrepeated19MinTB  = subDict['numrepeated19MinTB']
 
     # deprecated (Pete fixed code)
-    #if vFlag==1:
-    #    print('\n    #TB with SchBasal before     : {:5d}'.format(numSchBasalbeforeTB))
+    # if vFlag==1:
+    #    print('\n    #TB with SchBasal before     : {:5d}'.format(
+    #           numSchBasalbeforeTB))
     #    print('    #TB sent at <30s interval    : {:5d}'.format(numShortTB))
-    #    print('    #TB repeated value           : {:5.0f}'.format(numRepeatedTB))
-    #    print('    #TB repeated value <30s      : {:5.0f}'.format(numRepeatedShortTB))
-    #    print('    #TB rep value >=30s & <19min : {:5.0f}'.format(numrepeated19MinTB))
+    #    print('    #TB repeated value           : {:5.0f}'.format(
+    #           numRepeatedTB))
+    #    print('    #TB repeated value <30s      : {:5.0f}'.format(
+    #           numRepeatedShortTB))
+    #    print('    #TB rep value >=30s & <19min : {:5.0f}'.format(
+    #           numrepeated19MinTB))
     return
 
+
 def printInitFrame(podInitFrame):
-    print('\n  CumSec: seqNum: expectAction  : expMT  :success: actMT  : ' \
-           'actPP: ppMeaning')
+    print('\n  CumSec: seqNum: expectAction  : expMT  :success: actMT  : '
+          'actPP: ppMeaning')
     for index, row in podInitFrame.iterrows():
-        print('  {:5.0f}: {:7d}: {:14s}: {:7s}:    {:3s}: {:7s}: ' \
-            '{:5d}: {:14s}'.format(row['timeCumSec'],
-            row['seqNum'], row['expectAction'], row['expectMT'],
-            getStringFromLogic(row['statusBool']), row['actualMT'],
-            row['actualPP'], row['ppMeaning']))
+        print('  {:5.0f}: {:7d}: {:14s}: {:7s}:    {:3s}: {:7s}: '
+              '{:5d}: {:14s}'.format(
+               row['timeCumSec'], row['seqNum'], row['expectAction'],
+               row['expectMT'], getStringFromLogic(row['statusBool']),
+               row['actualMT'], row['actualPP'], row['ppMeaning']))
     return
+
 
 def printPodInfo(podInfo, nomNumSteps):
     if 'rssiValue' in podInfo:
         # print('\n')
-        if podInfo['numInitSteps'] > nomNumSteps:
-            print('    *** pod exceeded nominal init steps of {:d}' \
-                  ' ***'.format(nomNumSteps))
-        print('    Pod: Address {:s}, Lot {:s}, PI: {:s}, gain {:d}, rssi {:d}, ' \
-              'numInitSteps {:d}'.format(podInfo['address'], podInfo['lot'],
-              podInfo['piVersion'],
-              podInfo['recvGain'], podInfo['rssiValue'], podInfo['numInitSteps']))
+        if 'numInitSteps' in podInfo:
+            if podInfo['numInitSteps'] > nomNumSteps:
+                print('    *** pod exceeded nominal init steps of {:d}'
+                      ' ***'.format(nomNumSteps))
+            print('   Pod: Addr {:s}, Lot {:s}, PI: {:s}, gain {:d}, rssi {:d}'
+                  ', numInitSteps {:d}'.format(
+                    podInfo['address'], podInfo['lot'], podInfo['piVersion'],
+                    podInfo['recvGain'], podInfo['rssiValue'],
+                    podInfo['numInitSteps']))
+        else:
+            printDict(podInfo)
     return
+
 
 def printLogInfoSummary(logInfoDict):
     #
@@ -69,29 +85,44 @@ def printLogInfoSummary(logInfoDict):
     # need this True to get the actionSummary used to fill csv file
     podOnTimeString = ''
     if logInfoDict.get('podOnTime'):
-        podOnTimeString = '            Pod active time (hrs) : {:6.1f}'.format(logInfoDict['podOnTime']/60)
+        podOnTimeString = 'Pod active time (hrs) : {:6.1f}'.format(
+                           logInfoDict['podOnTime']/60)
     print('\n  This pod status:')
-    print(podOnTimeString)
-    print('     Insulin delivered by pod (u) : {:7.2f} ({:s})'.format(logInfoDict['insulinDelivered'], logInfoDict['sourceString']))
+    print('           ', podOnTimeString)
+    print('     Insulin delivered by pod (u) : {:7.2f} ({:s})'.format(
+           logInfoDict['insulinDelivered'], logInfoDict['sourceString']))
     if logInfoDict.get('logFileHasInit'):
         print('\n  Report below is for all messages to date for this pod')
     else:
-        print('\n  Report below is for the subset of messages contained in the log')
+        print('\n  Report below is for the subset of messages contained '
+              'in the log')
     print('\n            First message in log :', logInfoDict['first_msg'])
     print('            Last  message in log :', logInfoDict['last_msg'])
-    print('  Total elapsed time in log (hrs) : {:6.1f}'.format(logInfoDict['msgLogHrs']))
-    print('                Radio on estimate : {:6.1f}, {:5.1f}%'.format(logInfoDict['radioOnHrs'], 100*logInfoDict['radioOnHrs']/logInfoDict['msgLogHrs']))
-    print('   Number of messages (sent/recv) :{:5d} ({:4d} / {:4d})'.format(logInfoDict['numMsgs'],
-        logInfoDict['send_receive_messages'][1], logInfoDict['send_receive_messages'][0]))
-    print('    Messages in completed actions :{:5d} : {:.1f}%'.format( \
-        logInfoDict['totalCompletedMessages'], logInfoDict['percentCompleted']))
-    print('          Number of nonce resyncs :{:5d}'.format(logInfoDict['numberOfNonceResync']))
-    print('       Total Bolus Req in log (u) : {:7.2f}'.format(logInfoDict['totBolus']))
+    print('  Total elapsed time in log (hrs) : {:6.1f}'.format(
+           logInfoDict['msgLogHrs']))
+    print('                Radio on estimate : {:6.1f}, {:5.1f}%'.format(
+           logInfoDict['radioOnHrs'],
+           100*logInfoDict['radioOnHrs']/logInfoDict['msgLogHrs']))
+    print('   Number of messages (sent/recv) :{:5d} ({:4d} / {:4d})'.format(
+           logInfoDict['numMsgs'], logInfoDict['send_receive_messages'][1],
+           logInfoDict['send_receive_messages'][0]))
+    print('    Messages in completed actions :{:5d} : {:.1f}%'.format(
+           logInfoDict['totalCompletedMessages'],
+           logInfoDict['percentCompleted']))
+    print('          Number of nonce resyncs :{:5d}'.format(
+           logInfoDict['numberOfNonceResync']))
+    print('       Total Bolus Req in log (u) : {:7.2f}'.format(
+           logInfoDict['totBolus']))
     if 'autB' in logInfoDict:
         if logInfoDict['manB'] < logInfoDict['totBolus']:
-            print('                       Manual (u) : {:7.2f}, {:3.0f} %'.format(logInfoDict['manB'], 100*logInfoDict['manB']/logInfoDict['totBolus']))
-            print('                    Automatic (u) : {:7.2f}, {:3.0f} %'.format(logInfoDict['autB'], 100*logInfoDict['autB']/logInfoDict['totBolus']))
+            print('                  Manual (u) : {:7.2f}, ''{:3.0f} %'.format(
+                   logInfoDict['manB'],
+                   100*logInfoDict['manB']/logInfoDict['totBolus']))
+            print('               Automatic (u) : {:7.2f}, {:3.0f} %'.format(
+                   logInfoDict['autB'],
+                   100*logInfoDict['autB']/logInfoDict['totBolus']))
     return
+
 
 def printLoopVersion(loopVersionDict):
     print('  ----------------------------------------')
@@ -102,11 +133,12 @@ def printLoopVersion(loopVersionDict):
     print('     sourceRoot : {:s}'.format(loopVersionDict['sourceRoot']))
     return
 
+
 def writePodInfoToOutputFile(outFile, lastDate, thisFile, podInfo):
     # check if file exists
     isItThere = os.path.isfile(outFile)
     # now open the file
-    stream_out = open(outFile,mode='at')
+    stream_out = open(outFile, mode='at')
     if not isItThere:
         # set up a table format order
         headerString = 'date, filename, lot, rssi, numInitSteps'
@@ -120,13 +152,16 @@ def writePodInfoToOutputFile(outFile, lastDate, thisFile, podInfo):
     stream_out.write('\n')
     stream_out.close()
 
+
 def printPodDict(podDict):
     # print a few things then returns
     # add protection here
     if 'address' in podDict:
         print('    Pod: Address {:s}'.format(podDict['address']))
     else:
-        print('      Missing podDict, check report for ## OmnipodPumpManager section')
+        print('      Missing podDict, check report for ## OmnipodPumpManager '
+              'section')
+
 
 def printFrameDebug(frame):
     print('\n Debug printout of head and tail')
@@ -136,19 +171,49 @@ def printFrameDebug(frame):
     print('\n')
     return
 
+
 def writePodInitStateToOutputFile(outFile, commentString, podInitState):
-    print('\n *** Sending podInitState {:s}, to \n     {:s}'.format(commentString, outFile))
+    print('\n *** Sending podInitState {:s}, to \n     {:s}'.format(
+           commentString, outFile))
     # select the desired columns and order for output
-    columnList = ['logIdx','timeStamp','deltaSec','timeCumSec',
-        'seqNum','expectAction','expectMT','expectPP','success',
-        'actualMT','actualPP','ppMeaning','address','msgDict']
-    podInitState['success'] = podInitState['statusBool'].apply(getStringFromLogic)
+    columnList = ['logIdx', 'timeStamp', 'deltaSec', 'timeCumSec',
+                  'seqNum', 'expectAction', 'expectMT', 'expectPP', 'success',
+                  'actualMT', 'actualPP', 'ppMeaning', 'address', 'msgDict']
+    podInitState['success'] = podInitState['statusBool'].apply(
+                              getStringFromLogic)
     podInitState = podInitState[columnList]
     podInitState.to_csv(outFile)
     return
 
+
+def reportUncategorizedMessages(frameBalance, podState):
+    # used to report uncategorized messages
+    if len(frameBalance) == 0:
+        return
+
+    print('\n *** There were {:d} messages not in completed Actions'.format(
+           len(frameBalance)))
+    # extract msgType and number of occurences
+    nonCatDict = frameBalance.groupby(['msgType']).groups
+    print('       msgType     : Meaning             : #NotCategorized')
+    for keys, values in nonCatDict.items():
+        print('        {:10s} :  {:18s} :     {:3d}'.format(
+               keys, getNameFromMsgType(keys), len(values)))
+    print('\n')
+
+    frameBalance['description'] = frameBalance.apply(
+                  lambda row: getDescriptiveStringFromPodStateRow(
+                   row['msgDict'], row['reqTB'], row['reqBolus'],
+                   row['pod_progress']), axis=1)
+    columnList = ['timeStamp', 'deltaSec', 'pod_progress', 'type', 'msgType',
+                  'description']
+    frameBalance = frameBalance[columnList]
+    return
+
+
 def writePodStateToOutputFile(outFile, commentString, podState, logInfoDict):
-    print('\n *** Sending podState {:s}, to \n     {:s}'.format(commentString, outFile))
+    print('\n *** Sending podState {:s}, to \n     {:s}'.format(
+           commentString, outFile))
     # change the True False columns to 'y' and '' (make Joe happy)
     podState['TB'] = podState['TB'].apply(getStringFromLogic)
     podState['SchB'] = podState['SchBasal'].apply(getStringFromLogic)
@@ -156,39 +221,44 @@ def writePodStateToOutputFile(outFile, commentString, podState, logInfoDict):
     # select the desired columns and order for output
     if 'manB' in logInfoDict:
         podState['autoBolus'] = podState['autoBolus'].apply(getStringFromLogic)
-        columnList = ['logIdx','timeStamp','deltaSec','timeCumSec',
-            'radioOnCumSec','seqNum','pod_progress','type','msgType',
-            'insulinDelivered','reqTB','TB','SchB','reqBolus','Bolus',
-            'autoBolus','address','msgDict']
+        columnList = ['logIdx', 'timeStamp', 'deltaSec', 'timeCumSec',
+                      'radioOnCumSec', 'seqNum', 'pod_progress', 'type',
+                      'msgType', 'insulinDelivered', 'reqTB', 'TB', 'SchB',
+                      'reqBolus', 'Bolus', 'autoBolus', 'address', 'msgDict']
     else:
-        columnList = ['logIdx','timeStamp','deltaSec','timeCumSec',
-            'radioOnCumSec','seqNum','pod_progress','type','msgType',
-            'insulinDelivered','reqTB','TB','SchB','reqBolus','Bolus',
-            'address','msgDict']
+        columnList = ['logIdx', 'timeStamp', 'deltaSec', 'timeCumSec',
+                      'radioOnCumSec', 'seqNum', 'pod_progress', 'type',
+                      'msgType', 'insulinDelivered', 'reqTB', 'TB', 'SchB',
+                      'reqBolus', 'Bolus', 'address', 'msgDict']
     podState = podState[columnList]
     podState.to_csv(outFile)
     return
 
 
 def writeDescriptivePodStateToOutputFile(outFile, commentString, podState):
-    print('\n *** Sending descriptivePodState {:s}, to \n     {:s}'.format(commentString, outFile))
+    print('\n *** Sending descriptivePodState {:s}, to \n     {:s}'.format(
+           commentString, outFile))
     # create descriptive string based on message
-    podState['description'] = podState.apply(lambda row : getDescriptiveStringFromPodStateRow(row['msgDict'],
-                                  row['reqTB'], row['reqBolus'], row['pod_progress']), axis = 1)
+    podState['description'] = \
+        podState.apply(lambda row: getDescriptiveStringFromPodStateRow(
+                row['msgDict'], row['reqTB'], row['reqBolus'],
+                row['pod_progress']), axis=1)
     # select the desired columns and order for output
     podState['timeCumMin'] = podState['timeCumSec'].apply(minStrFromSec)
     verboseFlag = 1
     # add ,'msgDict' if want more verbosity
     if verboseFlag:
-        columnList = ['timeStamp','deltaSec','timeCumMin',
-            'seqNum','address','pod_progress',
-            'type','msgType', 'insulinDelivered','msgDict','description']
+        columnList = ['timeStamp', 'deltaSec', 'timeCumMin', 'seqNum',
+                      'address', 'pod_progress', 'type', 'msgType',
+                      'insulinDelivered', 'msgDict', 'description']
     else:
-        columnList = ['timeStamp','deltaSec','timeCumMin',
-            'pod_progress', 'msgType', 'insulinDelivered','msgDict','description']
+        columnList = ['timeStamp', 'deltaSec', 'timeCumMin',
+                      'pod_progress', 'msgType', 'insulinDelivered', 'msgDict',
+                      'description']
     podState = podState[columnList]
     podState.to_csv(outFile)
     return
+
 
 def getStringFromLogic(bool):
     if bool:
@@ -196,15 +266,17 @@ def getStringFromLogic(bool):
     else:
         return ''
 
+
 def minStrFromSec(value):
     minStr = '{:.1f}'.format(value / 60)
     return minStr
+
 
 def writePodox0115ToOutputFile(outFile, thisFile, pod0x0115Response):
     # check if file exists
     isItThere = os.path.isfile(outFile)
     # now open the file
-    stream_out = open(outFile,mode='at')
+    stream_out = open(outFile, mode='at')
     if not isItThere:
         # set up a table format order
         headerString = 'time, cumSec, #07toPod, #03toPod, #0115frmPod, ' + \
@@ -229,11 +301,12 @@ def writePodox0115ToOutputFile(outFile, thisFile, pod0x0115Response):
         stream_out.write(f'{thisFile}\n')
     stream_out.close()
 
+
 def writeCombinedLogToOutputFile(outFile, logDF):
     # print('\n *** Sending Report Dataframe to \n     {:s}'.format(outFile))
     # print('   input columns', logDF.columns)
-    columnList = ['time','deltaSec','address','seqNum',
-            'type','msgDict']
+    columnList = ['time', 'deltaSec', 'address', 'seqNum',
+                  'type', 'msgDict']
     logDF = logDF[columnList]
     logDF.to_csv(outFile)
     return
