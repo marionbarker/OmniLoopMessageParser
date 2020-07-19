b1255b5a3213ddfbc5860a37ac6073f27b8192ad
marionbarker <marionbarker@earthlink.net>
2020-07-19 12:28:58 -0700

    clean up format (flake8)

--- parsers/parse_03.py
+++ parsers/parse_03.py
@@ -1,7 +1,8 @@
-# file: parse_03 - setup pod, follows successful 07 (assignID) and 0x0115 response
+# file: parse_03 - setup pod, follows successful 07 (assignID) and 0x0115
 #  wiki: https://github.com/openaps/openomni/wiki/Command-03-Setup-Pod
 
 from util.misc import combineByte
+
 
 def parse_03(byteList, msgDict):
     # Command to pod to finish initialization of address, TID, Lot
@@ -17,8 +18,9 @@ def parse_03(byteList, msgDict):
     LL (1 byte) [1]: mlen $13 for typical case or $0b
     IIIIIIII (4 bytes) [2:5]: Pod ID as given in the 07 Command
     NU (1 byte) [6]: Not Used
-    TO (1 byte) [7]: Packet Timeout Limit (max 50, 0 sets the default value of 4)
-    MMDDYY (3 bytes) [8:$A]: MM: month (1-12), DD: day (1-31), YY: years since 2000
+    TO (1 byte) [7]: Pkt Timeout Limit (max 50, 0 sets the default value of 4)
+    MMDDYY (3 bytes) [8:$A]: MM: month (1-12), DD: day (1-31),
+                             YY: years since 2000
     HHMM (2 bytes) [$B:$C]: HH: hour (0-23), MM minutes (0-59)
     LLLLLLLL (4 bytes) [$D:$10]: Lot (long format only)
     TTTTTTTT (4 bytes) [$11:$14]: TID (long format only)
@@ -40,14 +42,14 @@ def parse_03(byteList, msgDict):
     podLot = combineByte(byteList[13:17])
     podTid = combineByte(byteList[17:21])
 
-    msgDict['podAddr']  = hex(podAddr)
-    msgDict['timeOut']  = timeOut
-    msgDict['dateStamp']  = '{0:#0{1}d}'.format(MM,2) + '/' + \
-                            '{0:#0{1}d}'.format(DD,2) + '/' + \
-                            '20{0:#0{1}d}'.format(YY,2) + ' ' + \
-                            '{0:#0{1}d}'.format(hh,2) + ':' + \
-                            '{0:#0{1}d}'.format(mm,2)
-    msgDict['lot']  = podLot
-    msgDict['tid']  = podTid
+    msgDict['podAddr'] = hex(podAddr)
+    msgDict['timeOut'] = timeOut
+    msgDict['dateStamp'] = '{0:#0{1}d}'.format(MM, 2) + '/' + \
+                           '{0:#0{1}d}'.format(DD, 2) + '/' + \
+                           '20{0:#0{1}d}'.format(YY, 2) + ' ' + \
+                           '{0:#0{1}d}'.format(hh, 2) + ':' + \
+                           '{0:#0{1}d}'.format(mm, 2)
+    msgDict['lot'] = podLot
+    msgDict['tid'] = podTid
 
     return msgDict
