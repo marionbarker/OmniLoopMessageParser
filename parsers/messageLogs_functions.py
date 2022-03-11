535baf44cd4caf3398e8238e25babb1c2f899c45
marionbarker <marionbarker@earthlink.net>
2022-03-11 08:04:48 -0800

    simplify FAX parsing, look for string without leading number

--- parsers/messageLogs_functions.py
+++ parsers/messageLogs_functions.py
@@ -440,16 +440,10 @@ def extract_raw_pod(raw_content):
     # Need to come back and fix this, but skip them to avoid problems
     #  when splitting logDF into separate pods later
     # At some point in time, FAX started using a different pattern
-    pod_patt = "318 - DEV: Device message:"
+    pod_patt = "DEV: Device message:"
     pod_messages = [x for x in lines_raw if x.find(pod_patt) > -1]
     if len(pod_messages) == 0:
-        pod_patt = "385 - DEV: Device message:"
-        pod_messages = [x for x in lines_raw if x.find(pod_patt) > -1]
-        if len(pod_messages) == 0:
-            pod_patt = "417 - DEV: Device message:"
-            pod_messages = [x for x in lines_raw if x.find(pod_patt) > -1]
-            if len(pod_messages) == 0:
-                return logDF
+        return logDF
     if noisy:
         print('first pod line\n', pod_messages[0][0:19], ' ',
               pod_messages[0][166:])
